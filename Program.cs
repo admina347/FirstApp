d1d6fc2fcb715c328d1c4886087b8c561a0de4da
admina347 <it-mactep@mail.ru>
2022-08-14 12:58:03 +0500

    Exercise 4.1.17, 4.1.18

--- Program.cs
+++ Program.cs
@@ -2,13 +2,20 @@
 
 class MainClass
 {
-	public static void Main(string[] args)
-	{
+    public static void Main(string[] args)
+    {
 
-		string myName = "Ivan";
+		Console.Write("Введите имя: ");
+		var name = Console.ReadLine();
+		Console.Write("Введите возраст: ");
+		var age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+		Console.Write("Введите дату рождения: ");
+		var birthdate = Console.ReadLine();
+		Console.WriteLine("Your birthdate is: {0}", birthdate);
 
 		Console.WriteLine(myName);
 
-		Console.ReadKey();
-	}
+        Console.ReadKey();
+    }
 }
