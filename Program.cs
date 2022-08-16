cbdd371c2c12f0cddf0cd821874eedb4478a4eb7
admina347 <it-mactep@mail.ru>
2022-08-16 11:41:25 +0500

    unit-4-4-2

--- Program.cs
+++ Program.cs
@@ -4,16 +4,12 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-
+		(string name, int age) anketa;
 		Console.Write("Введите имя: ");
-		var name = Console.ReadLine();
+		anketa.name = Console.ReadLine();
 		Console.Write("Введите возраст: ");
-		var age = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-		Console.Write("Введите дату рождения: ");
-		var birthdate = Console.ReadLine();
-		Console.WriteLine("Your birthdate is: {0}", birthdate);
-
+		anketa.age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Your name is {0} and age is {1} ", anketa.name, anketa.age);
 
 		Console.ReadKey();
 	}
